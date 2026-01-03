be322bb5b718c229489298d619814c76b06259c0
yasaswinireddy119 <yasaswinireddykotha@gmail.com>
2026-01-03 19:01:52 +0530

    Mini Restaurant Management App - (Role Based) with the react

--- Mini Restaurant Management App /src/Pages/Login.jsx
+++ Mini Restaurant Management App /src/Pages/Login.jsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react";
+
 function Login(){
-    return()
+    const[email,setEmail]=useState("")
+    const[password,setPassword]=useState("")
+    navigate=useNavigate()
+    const[message,setMessage]=useState("")
+    const emailRef=useRef(null)
+    useEffect(()=>{
+        emailRef.current.focus();
+    },[])
+    const handlelogin=(e)=>{
+        e.preventDefault();
+        if(e)
+    }
+    return(
+
+    )
 }
 export default Login;
